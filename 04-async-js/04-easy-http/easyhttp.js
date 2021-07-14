77e3d5f48e58018587102eabdec377faf41bf399
Sattvik Palta <sattvikpalta@gmail.com>
2021-07-15 00:16:16 +0530

    Custom HTTP library (AJAX with callbacks)

--- 04-async-js/04-easy-http/easyhttp.js
+++ 04-async-js/04-easy-http/easyhttp.js
@@ -19,7 +19,31 @@ easyHTTP.prototype.get = function (url, callback) {
 }
 
 // Make an HTTP POST request
+easyHTTP.prototype.post = function (url, data, callback) {
+  this.http.open('POST', url, true);
+
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  }
+
+  this.http.send(JSON.stringify(data));
+}
 
 // Make an HTTP PUT request
+easyHTTP.prototype.put = function (url, data, callback) {
+  this.http.open('PUT', url, true);
+
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  }
+
+  this.http.send(JSON.stringify(data));
+}
 
 // Make an HTTP DELETE request
