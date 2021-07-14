ef9f546fd750127e6f98eef321dc3115ddb7dbc6
Sattvik Palta <sattvikpalta@gmail.com>
2021-07-15 00:19:58 +0530

    Custom HTTP library (AJAX with callbacks)

--- 04-async-js/04-easy-http/app.js
+++ 04-async-js/04-easy-http/app.js
@@ -41,3 +41,12 @@ const data = {
 //     console.log(post);
 //   }
 // });
+
+// Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(response);
+  }
+});
